e70f62634ed62a5831cb4e1738c891803fd9fae7
zeddy5000 <zeedy5000@gmail.com>
2023-10-12 10:06:18 +0100

    updated

--- chat_project/settings.py
+++ chat_project/settings.py
@@ -73,6 +73,15 @@ TEMPLATES = [
 WSGI_APPLICATION = 'chat_project.wsgi.application'
 ASGI_APPLICATION = "chat_project.asgi.application"
 
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
+
 
 
 # Database
